b8e2d18dc6c1c6bc4c2b7131a5c3e03cb8ce38a2
Kevin Price <collegekevin@gmail.com>
2022-12-13 20:23:28 -0800

    commit tree cb234bdb8000cf85c6aaa9dd7773d7a457d7ac2b

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,15 @@
 class Post < ApplicationRecord
+    validates :title, presence: true
+    validates :content, length: { minimum: 250 }
+    validates :summary, length: { maximum: 250 }
+    validates :category, inclusion: {in: 'Fiction', 'Non-fiction' }
+
+    # validate :clickbaity?
+
+    # def clickbaity?
+    #    unless title.match? (["Won't Believe", "Secret", "Top", "Guess"])
+    #     errors.add(:title, "Insufficient Title.")
+    #    end
+    # end
+
 end
